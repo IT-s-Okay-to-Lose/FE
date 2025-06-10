9a22c429811387edb615bdd2bba546e8394f0674
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-10 15:26:24 +0900

    Desing: BoxTab.Lagrge 폰트 디자인 수정

--- src/shared/components/molecules/BoxTab.tsx
+++ src/shared/components/molecules/BoxTab.tsx
@@ -43,10 +43,42 @@ const createRoundTab = (sizeClass: string) => {
   return Tab;
 };
 
+const createLargeRoundTab = (sizeClass: string) => {
+  const Tab: React.FC<ToggleTabProps> = ({
+    options,
+    value,
+    onChange,
+    className,
+  }) => (
+    <div
+      className={cn("flex w-fit p-1 rounded-md gap-2 bg-otl-sub", className)}
+    >
+      {options.map((option) => (
+        <button
+          key={option}
+          onClick={() => onChange(option)}
+          className={cn(
+            "rounded-md font-semibold text-otl-gray transition whitespace-nowrap flex justify-center items-center",
+            sizeClass,
+            option === value ? "bg-white shadow" : "hover:bg-white"
+          )}
+        >
+          <Typography.SubTitle1
+            className={cn(option === value ? "text-black" : "text-otl-gray")}
+          >
+            {option}
+          </Typography.SubTitle1>
+        </button>
+      ))}
+    </div>
+  );
+  return Tab;
+};
+
 const BoxTab = {
-  Default: createRoundTab("w-[115px] h-[40px] px-5 py-2 text-sm"),
-  Small: createRoundTab("w-[70px] h-[35px] px-3 py-1 text-xs"),
-  Large: createRoundTab("w-[130px] h-[45px] px-6 py-3 text-lg"),
+  Default: createRoundTab("w-[115px] h-[40px] px-5 py-2 "),
+  Small: createRoundTab("w-[70px] h-[35px] px-3 py-1 "),
+  Large: createLargeRoundTab("w-[130px] h-[45px] px-6 py-3 "),
 };
 
 export default BoxTab;
