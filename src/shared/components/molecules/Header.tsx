f4d95d6cf7f42560c53efd56b49f6742a6699212
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-10 16:21:26 +0900

    Design: pathname 추출 후 헤더에 디자인 적용

--- src/shared/components/molecules/Header.tsx
+++ src/shared/components/molecules/Header.tsx
@@ -1,23 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Typography from "../atoms/Typography";
 
 function Header() {
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const isActive = (path: string) => pathname === path;
+
   return (
     <div className="flex justify-center">
       <div className="flex w-full max-w-[850px] laptop:max-w-[1150px] px-6 py-3 justify-between items-center">
         <Typography.SubTitle1>IT{"'"}s Okay to Lose</Typography.SubTitle1>
 
-        <div className="flex gap-7 items-center ">
+        <div className="flex gap-7 items-center">
           <Link to="/">
-            <Typography.SubTitle2>홈</Typography.SubTitle2>
+            <Typography.SubTitle2
+              className={isActive("/") ? "font-bold" : "font-normal"}
+            >
+              홈
+            </Typography.SubTitle2>
           </Link>
           <Link to="/dashboard">
-            <Typography.SubTitle2>대시보드</Typography.SubTitle2>
+            <Typography.SubTitle2
+              className={isActive("/dashboard") ? "font-bold" : "font-normal"}
+            >
+              대시보드
+            </Typography.SubTitle2>
           </Link>
           <Link to="/overview">
-            <Typography.SubTitle2>투자 모아보기</Typography.SubTitle2>
+            <Typography.SubTitle2
+              className={isActive("/overview") ? "font-bold" : "font-normal"}
+            >
+              투자 모아보기
+            </Typography.SubTitle2>
           </Link>
         </div>
+
         <div className="w-[155px] flex justify-end">
           <div className="border w-[40px] h-[40px] rounded-full bg-otl-gray"></div>
         </div>
