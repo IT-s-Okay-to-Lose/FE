11f5634486a349d9d34521a908d3d47146599443
beenvyn <beenvyn5262@gmail.com>
2025-06-10 11:48:17 +0900

    Feat: Button disabled를 props로 받도록 수정

--- src/shared/components/atoms/Button.tsx
+++ src/shared/components/atoms/Button.tsx
@@ -12,6 +12,9 @@ const DefaultButtonClass =
 const SmallButtonClass =
   "box-border border border-gray-200 transition-all duration-200 flex items-center justify-center gap-2 w-[115px] h-[40px]";
 
+const DisabledButtonClass = (baseClass: string, disabled?: boolean) =>
+  disabled ? "bg-otl-disabled text-white" : baseClass;
+
 const createDefaultButton = (
   customClassName: string = "",
   defaultProps?: Partial<ButtonBaseProps>
@@ -19,11 +22,19 @@ const createDefaultButton = (
   const Component: FC<ButtonBaseProps> = ({
     children,
     className,
+    disabled,
     ...props
   }) => (
     <button
       {...defaultProps}
-      className={cn(DefaultButtonClass, customClassName, className)}
+      disabled={disabled}
+      className={cn(
+        DefaultButtonClass,
+        customClassName,
+        className,
+
+        DisabledButtonClass(customClassName, disabled)
+      )}
       {...props}
     >
       <Typography.SubTitle1>{children}</Typography.SubTitle1>
@@ -40,11 +51,18 @@ const createSmallButton = (
   const Component: FC<ButtonBaseProps> = ({
     children,
     className,
+    disabled,
     ...props
   }) => (
     <button
       {...defaultProps}
-      className={cn(SmallButtonClass, customClassName, className)}
+      disabled={disabled}
+      className={cn(
+        SmallButtonClass,
+        customClassName,
+        className,
+        DisabledButtonClass(customClassName, disabled)
+      )}
       {...props}
     >
       <Typography.P2>{children}</Typography.P2>
@@ -58,41 +76,29 @@ const Button = {
   Main: createDefaultButton("bg-otl-main text-white rounded-md"),
   Sub: createDefaultButton("bg-otl-sub text-otl-gray rounded-md"),
   Point: createDefaultButton("bg-otl-point text-white rounded-md"),
-  Disabled: createDefaultButton("bg-otl-disabled text-white rounded-md", {
-    disabled: true,
-  }),
 
   Round: {
     Main: createDefaultButton("bg-otl-main text-white rounded-full"),
     Sub: createDefaultButton("bg-otl-sub text-otl-gray rounded-full"),
     Point: createDefaultButton("bg-otl-point text-white rounded-full"),
-    Disabled: createDefaultButton("bg-otl-disabled text-white rounded-full", {
-      disabled: true,
-    }),
   },
   Small: {
     Main: createSmallButton("bg-otl-main text-white rounded-md"),
     Sub: createSmallButton("bg-otl-sub text-otl-gray rounded-md"),
     Point: createSmallButton("bg-otl-point text-white rounded-md"),
-    Disabled: createSmallButton("bg-otl-disabled text-white rounded-md", {
-      disabled: true,
-    }),
   },
 };
 
 Button.Main.displayName = "Button.Main";
 Button.Sub.displayName = "Button.Sub";
 Button.Point.displayName = "Button.Point";
-Button.Disabled.displayName = "Button.Disabled";
 
 Button.Round.Main.displayName = "Button.Round.Main";
 Button.Round.Sub.displayName = "Button.Round.Sub";
 Button.Round.Point.displayName = "Button.Round.Point";
-Button.Round.Disabled.displayName = "Button.Round.Disabled";
 
 Button.Small.Main.displayName = "Button.Small.Main";
 Button.Small.Sub.displayName = "Button.Small.Sub";
 Button.Small.Point.displayName = "Button.Small.Point";
-Button.Small.Disabled.displayName = "Button.Small.Disabled";
 
 export default Button;
