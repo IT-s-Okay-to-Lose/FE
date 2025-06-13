eef138511f1bc8e6cd37c4fd0fc359f1f55fa818
beenvyn <beenvyn5262@gmail.com>
2025-06-13 23:36:39 +0900

    Design: 수익 그래프 퍼블리싱

--- src/features/profitGraph/ui/ProfitGraph.tsx
+++ src/features/profitGraph/ui/ProfitGraph.tsx
@@ -1,6 +1,13 @@
 import Typography from "@/shared/components/atoms/Typography";
 import RoundTab from "@/shared/components/molecules/RoundTab";
+import { mockProfitData } from "@/entities/stock/stock.mock";
+
 import { useState } from "react";
+import ReactECharts from "echarts-for-react";
+
+interface LineGraphProps {
+  data: [string, number][]; // date, profit
+}
 
 function ProfitGraph() {
   const filteringList = ["1주", "1달", "3달", "1년"];
@@ -9,14 +16,63 @@ function ProfitGraph() {
   return (
     <div>
       <Typography.Head1>최근 수익 그래프</Typography.Head1>
-      <Typography.Head1>40,243,000원</Typography.Head1>
-      <RoundTab.Small
-        options={filteringList}
-        value={filterTab}
-        onChange={setFilterTab}
-      />
+      <Typography.Head2>40,243,000원</Typography.Head2>
+      <div className="flex flex-col gap-[15px]">
+        <LineGraph data={mockProfitData} />
+        <RoundTab.Small
+          options={filteringList}
+          value={filterTab}
+          onChange={setFilterTab}
+        />
+      </div>
     </div>
   );
 }
 
 export default ProfitGraph;
+
+function LineGraph({ data }: LineGraphProps) {
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: { link: [{ xAxisIndex: "all" }] },
+    },
+    xAxis: {
+      type: "time",
+      scale: true,
+      min: "dataMin",
+      max: "dataMax",
+    },
+    yAxis: {
+      type: "value",
+      axisLabel: {
+        formatter: (value: number) => `${value.toLocaleString()}원`,
+      },
+    },
+    series: [
+      {
+        type: "line",
+        name: "수익",
+        data: data.map(([time, profit]) => [time, profit]),
+        areaStyle: {
+          color: "rgba(0, 128, 255, 0.2)",
+        },
+        lineStyle: {
+          color: "#398DEE",
+        },
+        symbol: "circle",
+        symbolSize: 5,
+      },
+    ],
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+  };
+
+  return (
+    <ReactECharts option={option} style={{ height: "400px", width: "100%" }} />
+  );
+}
