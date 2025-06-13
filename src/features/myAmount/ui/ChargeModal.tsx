9c6caf351962b5bbbee38bbd65577ac1b755583e
beenvyn <beenvyn5262@gmail.com>
2025-06-13 23:35:08 +0900

    Design: 돈 채우기 모달 추가

---
+++ src/features/myAmount/ui/ChargeModal.tsx
@@ -0,0 +1,46 @@
+import Card from "@/shared/components/atoms/Card";
+import Typography from "@/shared/components/atoms/Typography";
+import Button from "@/shared/components/atoms/Button";
+import Input from "@/shared/components/atoms/Input";
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+function ChargeModal({ onClose }: ModalProps) {
+  const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    // 모달 바깥쪽 (배경) 클릭 시 닫기
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className="fixed inset-0 z-5 flex items-center justify-center bg-black/50"
+      onClick={handleBackgroundClick}
+    >
+      <Card className="w-[335px] h-[320px] justify-between bg-white">
+        <Card.Header className="flex justify-between items-center">
+          <Typography.Head3>돈 채우기</Typography.Head3>
+          <button onClick={onClose}>
+            <i className="bi bi-x-lg text-xl" />
+          </button>
+        </Card.Header>
+        <Card.Content className="gap-[15px]">
+          <Input className="grow" />
+          <Typography.Caption className="flex justify-end">
+            최대 금액 30,000,000원
+          </Typography.Caption>
+        </Card.Content>
+        <Card.Footer>
+          <Button.Main className="grow" onClick={onClose}>
+            돈 채우기
+          </Button.Main>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
+}
+
+export default ChargeModal;
