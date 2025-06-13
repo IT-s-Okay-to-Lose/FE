622b7fe549350ce504ee0abc228f412bf1b37009
beenvyn <beenvyn5262@gmail.com>
2025-06-13 13:08:31 +0900

    Design: 투자 모아보기 페이지 퍼블리싱

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,6 +1,11 @@
 import Header from "@/shared/components/molecules/Header";
 import ScreenTooSmall from "@/shared/components/organisms/ScreenTooSmall";
 import useMediaQuery from "@/shared/hooks/useMediaQuery";
+import UserInfo from "@/features/userInfo/ui/UserInfo";
+import InvestmentStatus from "@/features/investmentStatus/ui/InvestmentStatus";
+import ProfitGraph from "@/features/profitGraph/ui/ProfitGraph";
+import AvailableAmount from "@/features/myAmount/ui/AvailableAmount";
+import InvestmentAmount from "@/features/myAmount/ui/InvestmentAmount";
 
 function OverviewPage() {
   const isTabletOrAbove = useMediaQuery();
@@ -12,8 +17,20 @@ function OverviewPage() {
       <div className="mb-8">
         <Header />
       </div>
-      <div className="w-full m-auto flex justify-center mt-10 gap-[60px]">
-        <p>content</p>
+      <div className="w-full m-auto flex flex-col items-center mt-10 gap-[60px]">
+        <div className="w-full max-w-[1100px]">
+          <UserInfo />
+        </div>
+        <div className="w-full max-w-[1100px] flex flex-col gap-[60px]">
+          <div className="flex flex-col gap-[60px]">
+            <InvestmentStatus />
+            <ProfitGraph />
+          </div>
+          <div className="flex flex-col gap-10">
+            <AvailableAmount />
+            <InvestmentAmount />
+          </div>
+        </div>
       </div>
     </div>
   );
