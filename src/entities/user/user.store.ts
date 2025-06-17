944946342786b4dde391a1822ca8c2a31762d282
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-17 11:32:15 +0900

    Feat: user.store 변경
    sate, action 분리
    hydrate 변수를 넣어 초기값에 의존하는 경우 즉시 참조하지 않도록 변경
    accessToken을 store에 넣어 프론트단에서 관리하도록 변경

--- src/entities/user/user.store.ts
+++ src/entities/user/user.store.ts
@@ -1,18 +1,47 @@
 import { create } from "zustand";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import type { UserInfo } from "./user.entity";
 
-interface UserStore {
+type AuthState = {
+  hasHydrated: boolean; // hydration 완료 후 값을 사용하도록 하는 변수
   userInfo: UserInfo | null;
+  isLoggedIn: boolean;
+  accessToken: string;
+};
+interface AuthAction {
+  setHydrated: (hydrate: boolean) => void;
   setUserInfo: (info: UserInfo) => void;
   clearUserInfo: () => void;
-  isLoggedIn: boolean;
   setLoggedIn: (state: boolean) => void;
+  setAccessToken: (token: string) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
+const initialUserState: AuthState = {
+  hasHydrated: true,
   userInfo: null,
-  setUserInfo: (info: UserInfo) => set({ userInfo: info }),
-  clearUserInfo: () => set({ userInfo: null, isLoggedIn: false }),
   isLoggedIn: false,
-  setLoggedIn: (state: boolean) => set({ isLoggedIn: state }),
-}));
+  accessToken: "",
+};
+
+export const useUserStore = create<AuthState & AuthAction>()(
+  devtools(
+    persist(
+      (set) => ({
+        ...initialUserState,
+        setLoggedIn: (v: boolean) => set({ isLoggedIn: v }),
+        setAccessToken: (token: string) => set({ accessToken: token }),
+        setUserInfo: (info: UserInfo) => set({ userInfo: info }),
+        setHydrated: (v: boolean) => set({ hasHydrated: v }),
+        clearUserInfo: () => set(initialUserState),
+      }),
+      {
+        name: "auth",
+        storage: createJSONStorage(() => localStorage),
+        onRehydrateStorage: () => (state) => {
+          state?.setHydrated?.(true);
+        },
+      }
+    ),
+    { name: "authStore" }
+  )
+);
