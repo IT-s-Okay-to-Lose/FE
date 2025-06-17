749b973f48051b1cd552cff62d55a596f26f57ac
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-18 00:09:52 +0900

    Feat: 주식 구매/판매 방식 변경
    주문 유형, % 선택 탭 삭제

--- src/shared/components/atoms/UnitInput.tsx
+++ src/shared/components/atoms/UnitInput.tsx
@@ -19,15 +19,16 @@ const UnitInput = ({ className, unit, ...props }: UnitInputProps) => {
     <div
       className={cn(
         "relative flex gap-2 pr-5 focus-within:pr-[19px] items-center rounded-md box-border border-[1px] border-otl-sub bg-white",
-        "focus-within:border-2 focus-within:border-otl-main",
-        props
+        "focus-within:border-2 focus-within:border-otl-main"
       )}
+      {...props}
     >
       <input
         className={cn(
           "w-[270px] h-[50px] focus:h-[48px] rounded-md border-none bg-white p-3 text-[16px] text-right font-semibold text-black placeholder:text-otl-gray focus:outline-none",
           className
         )}
+        {...props}
       />
       <Typography.SubTitle2>{unit}</Typography.SubTitle2>
     </div>
