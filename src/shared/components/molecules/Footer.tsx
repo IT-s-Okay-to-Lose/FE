0ac8d4de5ebeeac461dd417d6c3831491ccf10e1
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-17 14:37:33 +0900

    Design: footer 디자인 수정

--- src/shared/components/molecules/Footer.tsx
+++ src/shared/components/molecules/Footer.tsx
@@ -2,10 +2,10 @@ import Typography from "../atoms/Typography";
 
 function Footer() {
   return (
-    <div className="w-full h-[120px] flex flex-col justify-center bg-gray-50 mt-[50px] px-[160px]">
-      <div className="max-w-[850px] laptop:max-w-[1150px] m-auto">
+    <div className="w-full h-[160px] flex flex-col justify-center bg-gray-50 mt-[50px]">
+      <div className="w-[1150px] px-6  m-auto">
         <Typography.SubTitle1>IT{"'"}s Okay to Lose</Typography.SubTitle1>
-        <div className="border border-black/30 my-4" />
+        <div className="border-b-[1px] border-black/30 my-4" />
         <div className="flex justify-between items-center">
           <i className="bi bi-github text-2xl" />
           <Typography.Caption>
