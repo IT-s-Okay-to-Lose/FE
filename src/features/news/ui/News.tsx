714223278287257d40c4f68a193038bea912084a
Ian <105128049+minijae011030@users.noreply.github.com>
2025-06-23 15:30:04 +0900

    Feat: test

--- src/features/news/ui/News.tsx
+++ src/features/news/ui/News.tsx
@@ -1,24 +1,23 @@
-import type { News } from "@/entities/news/news.entity";
-import { useNewsStore } from "@/entities/news/news.store";
 import Typography from "@/shared/components/atoms/Typography";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { getNewsWithCache } from "../services/news.service";
 
 export function News() {
-  const [news, setNews] = useState<News[]>([]);
-  const lastFetched = useNewsStore((state) => state.lastFetched);
+  // const [news, setNews] = useState<News[]>([]);
+  // const lastFetched = useNewsStore((state) => state.lastFetched);
 
   async function getNewsFunction() {
     const result = await getNewsWithCache();
-    setNews(result);
+    console.log(result);
+    // setNews(result);
   }
 
-  const formattedTime = lastFetched
-    ? new Date(lastFetched).toLocaleTimeString("ko-KR", {
-        hour: "2-digit",
-        minute: "2-digit",
-      })
-    : "";
+  // const formattedTime = lastFetched
+  //   ? new Date(lastFetched).toLocaleTimeString("ko-KR", {
+  //       hour: "2-digit",
+  //       minute: "2-digit",
+  //     })
+  //   : "";
 
   useEffect(() => {
     getNewsFunction();
@@ -29,7 +28,7 @@ export function News() {
       <Typography.Head2 className="w-full text-right mb-3">
         주요 뉴스
       </Typography.Head2>
-      <div className="flex flex-col gap-3">
+      {/* <div className="flex flex-col gap-3">
         {news.map((news) => (
           <NewsRow
             key={news.id}
@@ -41,18 +40,18 @@ export function News() {
         <Typography.P2 className="text-right text-gray-500">
           {formattedTime && `마지막 업데이트 시간 ${formattedTime}`}
         </Typography.P2>
-      </div>
+      </div> */}
     </div>
   );
 }
 
-function NewsRow({ id, title, press }: News) {
-  return (
-    <div key={id} className="flex gap-3 justify-between">
-      <div className="w-full text-right">
-        <Typography.P1>{title}</Typography.P1>
-        <Typography.P2 className="text-otl-gray">{press}</Typography.P2>
-      </div>
-    </div>
-  );
-}
+// function NewsRow({ id, title, press }: News) {
+//   return (
+//     <div key={id} className="flex gap-3 justify-between">
+//       <div className="w-full text-right">
+//         <Typography.P1>{title}</Typography.P1>
+//         <Typography.P2 className="text-otl-gray">{press}</Typography.P2>
+//       </div>
+//     </div>
+//   );
+// }
