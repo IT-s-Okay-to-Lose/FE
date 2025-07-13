8dffdaab50cedb8b6150435807363bb634c451bf
beenvyn <beenvyn5262@gmail.com>
2025-07-13 15:40:41 +0900

    Refactor: 대시보드 api 호출 url 변경

--- src/entities/user/user.api.ts
+++ src/entities/user/user.api.ts
@@ -1,6 +1,10 @@
 import { Method } from "@/shared/utils/fetcher";
 
 export const user = {
+  getUserInfo: () => ({
+    url: `${import.meta.env.VITE_APP_API_URL}/user/userinfo`,
+    method: Method.GET,
+  }),
   reissue: () => ({
     url: `${import.meta.env.VITE_APP_API_URL}/reissue`,
     method: Method.GET,
@@ -13,24 +17,24 @@ export const user = {
     url: `${import.meta.env.VITE_APP_API_URL}/api/charge`,
     method: Method.GET,
   }),
-  getInvestmentSummary: (userId: number) => ({
-    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/summary?userId=${userId}`,
+  getInvestmentSummary: () => ({
+    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/summary`,
     method: Method.GET,
   }),
-  getHoldingRatio: (userId: number) => ({
-    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/holding-ratio?userId=${userId}`,
+  getHoldingRatio: () => ({
+    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/holding-ratio`,
     method: Method.GET,
   }),
-  getRealizedSummary: (userId: number, year: number, month: number) => ({
-    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/realized-summary?userId=${userId}&year=${year}&month=${month}`,
+  getRealizedSummary: (year: number, month: number) => ({
+    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/realized-summary?year=${year}&month=${month}`,
     method: Method.GET,
   }),
-  getRealizedDetail: (userId: number, year: number, month: number) => ({
-    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/realized-detail?userId=${userId}&year=${year}&month=${month}`,
+  getRealizedDetail: (year: number, month: number) => ({
+    url: `${import.meta.env.VITE_APP_API_URL}/api/dashboard/realized-detail?year=${year}&month=${month}`,
     method: Method.GET,
   }),
   postCharge: () => ({
-    url: `${import.meta.env.VITE_APP_API_URL}/api/charge`, // 임시 url
+    url: `${import.meta.env.VITE_APP_API_URL}/api/accounts/deposit`,
     method: Method.POST,
   }),
 };
