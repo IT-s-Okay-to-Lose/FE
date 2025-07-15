87c777c1e3642b1a16f416415e746ba55d38e417
beenvyn <beenvyn5262@gmail.com>
2025-07-15 22:29:30 +0900

    Feat: 사용자 정보 불러오기 api 연결

--- src/features/user/userInfo/ui/UserInfo.tsx
+++ src/features/user/userInfo/ui/UserInfo.tsx
@@ -1,24 +1,27 @@
-import {
-  mockUserInfo,
-  mockUserPortfolioSummary,
-} from "@/entities/user/user.mock";
+import { mockUserPortfolioSummary } from "@/entities/user/user.mock";
 import Card from "@/shared/components/atoms/Card";
 import Typography from "@/shared/components/atoms/Typography";
 import cn from "@/shared/utils/cn";
 import { formatFullDateToKorean, formatNumber } from "@/shared/utils/format";
+import type { UserInfo } from "@/entities/user/user.entity";
+import { useUserStore } from "@/entities/user/user.store";
 
 function UserInfo() {
+  const { userInfo } = useUserStore();
+
+  if (!userInfo) return;
+
   return (
     <Card>
       <div className="flex">
         <div className="flex-shrink-0 w-[90px] h-[90px] rounded-full bg-otl-gray mr-6 overflow-hidden">
-          <img src={mockUserInfo.imageUrl} />
+          <img src={userInfo.profileImage} />
         </div>
         <div className="grow flex justify-between items-center">
           <Card.Content>
-            <Typography.Head2>{mockUserInfo.name}</Typography.Head2>
+            <Typography.Head2>{userInfo.name}</Typography.Head2>
             <Typography.SubTitle2 className="text-otl-gray">
-              가입일: {formatFullDateToKorean(mockUserInfo.joinDate)}
+              가입일: {formatFullDateToKorean(new Date(userInfo.createdAt))}
             </Typography.SubTitle2>
           </Card.Content>
           <Card.Content>
